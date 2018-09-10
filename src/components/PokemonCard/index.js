e8114aa57f3592288936a758f2b53823da296558
mmaciekk <mmaciekk@gmail.com>
2018-09-10 11:28:06 +0200

    Pokemons are now displayed in groups of four

--- src/components/PokemonCard/index.js
+++ src/components/PokemonCard/index.js
@@ -1,18 +1,15 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody,
-    CardTitle} from 'reactstrap';
+import {Card, CardImg, CardText, CardBody, CardTitle} from 'reactstrap';
 
 const PokemonCard = (props) => {
     return (
-        <div>
-            <Card>
+            <Card  margin-bottom="10px">
                 <CardImg top width="100%" src={props.img} alt={`${props.name} image`} />
                 <CardBody>
                     <CardTitle>#{props.num} {props.name}</CardTitle>
                     <CardText>{props.type.join(' ')}</CardText>
                 </CardBody>
             </Card>
-        </div>
     );
 };
 
--- src/containers/Content/index.js
+++ src/containers/Content/index.js
@@ -1,22 +1,35 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import PokemonCard from '../../components/PokemonCard'
+import {Container, Col, Row} from 'reactstrap'
 
 class Content extends Component {
     displayPokemons() {
         const pokemons = this.props.pokemons
+        const pokemonsFours = pokemons.map((x,i) => {
+            return i % 4 === 0 ? pokemons.slice(i, i+4) : null;
+        }).filter(x => x != null);
+
         return (
-            <div>
-                {pokemons.map(
-                    pokemon => (
-                        <PokemonCard key={pokemon.id} name={pokemon.name} num={pokemon.num} img={pokemon.img} type={pokemon.type} />
-                    )
-                )}
-            </div>
+            <Container>
+                {pokemonsFours.map((pokemonsFours, index) => {
+                    return (<Row key={index}>
+                        {pokemonsFours.map((pokemon,index) =>
+                            <Col key={index} xs={12} sm={12} md={3} lg={3}>
+                            <PokemonCard
+                                key={pokemon.id}
+                                name={pokemon.name}
+                                num={pokemon.num}
+                                img={pokemon.img}
+                                type={pokemon.type}/>
+                            </Col>)}
+                    </Row>);
+                })}
+            </Container>
         )
     }
 
 
-    render () {
+    render() {
         return (
             <div>
                 {this.displayPokemons()}
